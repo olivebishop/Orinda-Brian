b94cf23eb57444cd26cb6b50ab850161efe856aa
olivebishop <olivehendrilgen1@gmail.com>
2023-10-23 00:20:44 +0300

    added services section

--- app/components/About/About.jsx
+++ app/components/About/About.jsx
@@ -1,15 +1,38 @@
-import React from 'react'
+import React, { useEffect } from 'react';
+import 'aos/dist/aos.css'; // Import AOS CSS
+import AOS from 'aos';
+import styles from './About.module.css';
+import Image from 'next/image';
 
 function About() {
-  return (
-    <section className=''>
-      <div>
-      about
-    </div>
+  useEffect(() => {
+    AOS.init(); // Initialize AOS for animations
+  }, []);
 
+  return (
+    <section className={styles['about-section']}>
+      <div className={styles.container}>
+        <h1 className={styles.heading} data-aos='fade-up'>
+          Our Services
+        </h1>
+
+        <div className={styles.services}>
+          <div className={`${styles.service} ${styles['lg-xlg']}`} data-aos='fade-down'>
+            <img src="/images/outdoor.jpg" alt="Outdoor Shoots" />
+            <p>Outdoor Shots</p>
+          </div>
+          <div className={`${styles.service} ${styles['lg-xlg']}`} data-aos='fade-down' data-aos-delay='100'>
+            <img src="/images/indoor.jpg" alt="Indoor Shots" />
+            <p>Indoor Shots</p>
+          </div>
+          <div className={`${styles.service} ${styles['lg-xlg']}`} data-aos='fade-down' data-aos-delay='200'>
+            <img src="/images/videography.jpg" alt="Videography Reels" />
+            <p>Videography Reels</p>
+          </div>
+        </div>
+      </div>
     </section>
-  
-  )
+  );
 }
 
-export default About
+export default About;
